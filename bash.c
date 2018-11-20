3c2adb92c669ff04bfd36b366ec9b548ec376bd8
LarWong <lwong7@stuy.edu>
2018-11-20 11:47:57 -0500

    f

---
+++ bash.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<signal.h>
+#include<fcntl.h>
+#include<limits.h>
+#include<string.h>
+
+static void sig(int signo){
+    if (signo == SIGINT){
+      printf("\nInterupt\n");
+      exit(0);
+    }
+}
+
+char ** parse_args(char *line){
+  char ** arr = malloc(9000);
+  for(int i = 0; arr[i] = strsep(&line," ");i++);
+  return arr;
+}
+
+int main(){
+  signal(SIGINT,sig);
+  char *input = malloc(9000);
+  
+  while(1){
+
+    printf("\n->");
+    scanf("%s",input);
+    char ** args = parse_args(input);
+
+  }
+
+
+
+
+
+
+
+  return 0;
+}
